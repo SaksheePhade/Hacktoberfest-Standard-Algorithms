e0d2c29b39398e92e2b5416241bc09850a3da22c
Sakshee Phade <sakshee1120@gmail.com>
2020-10-07 00:37:15 +0530

    Update BinaryTree.cpp
    Binary tree Depth first traversal

--- Trees and Graphs/Trees Algos/BinaryTree.cpp
+++ Trees and Graphs/Trees Algos/BinaryTree.cpp
@@ -113,3 +113,58 @@ int main()
 
 	return 0;
 }
+
+/*
+sakshee@sakshee-Vostro-3580:~/Data Structures$ ./a.out
+
+	Enter data for node(-1 if NULL) : 1
+
+	Enter left child of node 1 : 
+	Enter data for node(-1 if NULL) : 2
+
+	Enter left child of node 2 : 
+	Enter data for node(-1 if NULL) : 3
+
+	Enter left child of node 3 : 
+	Enter data for node(-1 if NULL) : -1
+
+	Enter right child of node 3 : 
+	Enter data for node(-1 if NULL) : -1
+
+	Enter right child of node 2 : 
+	Enter data for node(-1 if NULL) : 4
+
+	Enter left child of node 4 : 
+	Enter data for node(-1 if NULL) : -1
+
+	Enter right child of node 4 : 
+	Enter data for node(-1 if NULL) : -1
+
+	Enter right child of node 1 : 
+	Enter data for node(-1 if NULL) : 5
+
+	Enter left child of node 5 : 
+	Enter data for node(-1 if NULL) : 6
+
+	Enter left child of node 6 : 
+	Enter data for node(-1 if NULL) : -1
+
+	Enter right child of node 6 : 
+	Enter data for node(-1 if NULL) : -1
+
+	Enter right child of node 5 : 
+	Enter data for node(-1 if NULL) : -1
+
+	=========================INORDER TRAVERSAL========================
+	3	2	4	1	6	5
+	==================================================================
+
+	=========================PREORDER TRAVERSAL=======================
+	1	2	3	4	5	6
+	==================================================================
+
+	=========================POSTORDER TRAVERSAL======================
+	3	4	2	6	5	1
+	==================================================================
+
+*/
